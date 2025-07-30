278497b621cf13ccea8ccc219311079982632dce
Zairus Bermillo <166342742+notZairus@users.noreply.github.com>
2025-07-30 08:26:09 +0800

    feature(CustomizeStrip): after taking pix user can now customize the strip and download it

---
+++ src/configs.tsx
@@ -0,0 +1,76 @@
+
+
+export const availableFrames = [
+  {
+    frameBG: "#FFFFFF",
+    fontColor: "#000000"
+  },
+  {
+    frameBG: "#000000",
+    fontColor: "#EEEEEE"
+  },
+  {
+    frameBG: "pink",
+    fontColor: "#EEEEEE"
+  },
+  {
+    frameBG: "yellow",
+    fontColor: "#000000"
+  },
+  {
+    frameBG: "#7495de",
+    fontColor: "#EEEEEE"
+  },
+  {
+    frameBG: "url(/bgs/_1.png)",
+    fontColor: "#000000"
+  },
+  {
+    frameBG: "url(/bgs/_2.png)",
+    fontColor: "#EEEEEE"
+  },
+  {
+    frameBG: "url(/bgs/_3.png)",
+    fontColor: "#EEEEEE"
+  },
+  {
+    frameBG: "url(/bgs/_4.jpg)",
+    fontColor: "#EEEEEE"
+  },
+  {
+    frameBG: "url(/bgs/_5.jpg)",
+    fontColor: "#EEEEEE"
+  },
+  {
+    frameBG: "url(/bgs/_6.png)",
+    fontColor: "#EEEEEE"
+  },
+  {
+    frameBG: "url(/bgs/_7.png)",
+    fontColor: "#EEEEEE"
+  },
+  {
+    frameBG: "url(/bgs/_8.png)",
+    fontColor: "#EEEEEE"
+  },
+  {
+    frameBG: "url(/bgs/_9.png)",
+    fontColor: "#000000"
+  },
+  {
+    frameBG: "url(/bgs/_10.png)",
+    fontColor: "#EEEEEE"
+  }
+];
+
+
+
+export const imageShapes = [
+    "",
+    "rounded",
+    "rounded-full",
+    "rounded rounded-tl-4xl",
+    "rounded rounded-tr-4xl",
+    "rounded rounded-bl-4xl",
+    "rounded rounded-br-4xl",
+]
--- src/pages/CustomizeStrip.tsx
+++ src/pages/CustomizeStrip.tsx
@@ -1,93 +1,91 @@
 import { useImageContext } from "@/contexts/ImageContext";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import { useLocation } from "wouter";
 import { cn } from "@/lib/utils";
+import { availableFrames, imageShapes } from "@/configs";
+import { Button } from "@/components/ui/button.tsx";
+import { Switch } from "@/components/ui/switch";
+import html2canvas from "html2canvas";
 
-
-
-const availableFrames = [
-  {
-    frameBG: "bg-white",
-    fontColor: "text-black"
-  },
-  {
-    frameBG: "bg-black",
-    fontColor: "text-white"
-  },
-  {
-    frameBG: "bg-pink-400",
-    fontColor: "text-white"
-  },
-  {
-    frameBG: "bg-yellow-300",
-    fontColor: "text-black"
-  },
-  {
-    frameBG: "bg-blue-300",
-    fontColor: "text-white"
-  },
-  {
-    frameBG: "bg-[url(/bgs/_1.png)]",
-    fontColor: "text-black"
-  },
-  {
-    frameBG: "bg-[url(/bgs/_2.png)]",
-    fontColor: "text-white"
-  },
-  {
-    frameBG: "bg-[url(/bgs/_3.png)]",
-    fontColor: "text-white"
-  },
-  {
-    frameBG: "bg-[url(/bgs/_4.jpg)]",
-    fontColor: "text-white"
-  },
-  {
-    frameBG: "bg-[url(/bgs/_5.jpg)]",
-    fontColor: "text-white"
-  },
-  {
-    frameBG: "bg-[url(/bgs/_6.png)]",
-    fontColor: "text-white"
-  },
-  {
-    frameBG: "bg-[url(/bgs/_7.png)]",
-    fontColor: "text-white"
-  },
-  {
-    frameBG: "bg-[url(/bgs/_8.png)]",
-    fontColor: "text-white"
-  },
-  {
-    frameBG: "bg-[url(/bgs/_9.png)]",
-    fontColor: "text-black"
-  },
-  {
-    frameBG: "bg-[url(/bgs/_10.png)]",
-    fontColor: "text-white"
-  }
-]
 
 
 function CustomizeStrip() {
+  const [, setLocation] = useLocation();
   const { images } = useImageContext();
   const [ frameBG, setFrameBG ] = useState(availableFrames[0]);
+  const [ imageShape, setImageShape ] = useState(imageShapes[0]); 
+  const [ showDate, setShowDate ] = useState(false);
+  const stripRef = useRef(null);
 
-  
+
+  function getFullDate() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth();
+    const date = now.getDate();
+
+    return `${date}/${month + 1}/${year}`;
+  }
+
+  async function downloadStrip() {
+    if (!stripRef.current) return;
+
+    const canvasStrip: any = await html2canvas(stripRef.current);
+    const a: any = document.createElement('a');
+    a.href = canvasStrip.toDataURL(('image/png'));
+    a.download = "shuttr.png";
+    a.target = "blank";
+    a.click();
+  }
+
+  useEffect(() => {
+    if (images.length < 1) {
+      setLocation('/camera');
+    }
+  }, [])
 
   return (
     <>
+      
       <div className="w-full">
         <div className="w-3/4 h-full mx-auto flex flex-col md:flex-row gap-9 md:gap-4 pt-12 pb-20 mt:pb-0">
           <div className="flex-1">
           
-            <div className={cn("w-[160px] md:w-[180px] mx-auto space-y-1 p-2 border", frameBG.frameBG)}>
-              { images.map((image: string) => (
-                  <div className="w-full aspect-15/9">
-                    <img src={image} className="w-full h-full object-fit"/>
-                  </div>
-                ))
-              }
-              <p className={cn("text-primary text-xs font-bold text-center mt-4", frameBG.fontColor)}>Shuttr.</p>
+            <div className="w-min border mx-auto">
+              <div 
+                className="w-[160px] md:w-[180px] mx-auto space-y-1 p-2"
+                ref={stripRef}
+                style={{
+                  background: frameBG.frameBG
+                }}
+              >
+                { images.map((image: string) => (
+                    <div className={cn("w-full aspect-15/9 overflow-hidden", imageShape)}>
+                      <img src={image} className={cn("w-full h-full object-fit")}/>
+                    </div>
+                  ))
+                }
+                <div className="mt-2" style={{
+                  background: frameBG.fontColor === "#000000" ? "#FFFFFF50" : "#00000050"
+                }}>
+                  <p 
+                    className="text-[8px] text-center font-semibold h-4"
+                    style={{
+                      color: frameBG.fontColor
+                    }}
+                  >
+                    { showDate && getFullDate() }
+                  </p>
+                  <p 
+                    className="text-xs font-bold text-center"
+                    style={{
+                      color: frameBG.fontColor
+                    }}
+                  >
+                    Shuttr.
+                  </p>
+                </div>
+              </div>
             </div>
 
           </div>
@@ -95,16 +93,47 @@ function CustomizeStrip() {
             <div className="w-full h-full">
               <h1 className="text-xl font-bold text-center">Customize your strip!</h1>
 
-              <div className="mt-4 border">
-                <h2>Frame: </h2>
-                <div className="max-w-full flex items-start justify-baseline gap-2 flex-wrap">
-                  { availableFrames.map((frame) => (
-                      <div className={cn("aspect-square w-10 rounded-full border bg-repeat", frame.frameBG)} 
-                        onClick={() => setFrameBG(frame)}
-                      />
-                    ))
-                  }
+              <div className="mx-auto w-[240px]">
+                <div className="mt-4">
+                  <h2>Frame: </h2>
+                  <div className="max-w-full flex items-start justify-baseline gap-2 flex-wrap">
+                    { availableFrames.map((frame) => (
+                        <div 
+                          className="aspect-square w-10 rounded-full border bg-repeat" 
+                          style={{
+                            background: frame.frameBG
+                          }}
+                          onClick={() => setFrameBG(frame)}
+                        />
+                      ))
+                    }
+                  </div>
                 </div>
+  
+                <div className="mt-4">
+                  <h2>Frame Shape: </h2>
+                  <div className="max-w-full flex items-start gap-2 flex-wrap">
+                    { imageShapes.map((shape) => (
+                        <div className={cn("aspect-square w-10 border bg-repeat bg-white", shape)} 
+                          onClick={() => setImageShape(shape)}
+                        />
+                      ))
+                    }
+                  </div>
+                </div>
+
+                <div className="mt-4 flex items-center gap-4">
+                  <h2>Display Date: </h2>
+                  <Switch
+                    checked={showDate}
+                    onCheckedChange={() => setShowDate(!showDate)}
+                  />
+                </div>
+
+                <div className="w-full bg-black/10 mt-8">
+                  <Button className="w-full" onClick={downloadStrip}>Download Strip</Button>
+                </div>
+
               </div>
 
             </div>
