b7b67c830cf044df99d09d834437072af3692984
Zairus Bermillo <166342742+notZairus@users.noreply.github.com>
2025-07-31 20:59:41 +0800

    fix: added a favicon

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,33 +1,51 @@
 import { Link } from "wouter";
 import React from "react";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
 
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 
     return (
         <>
-            <header className="w-full py-4 h-16 justify-between items-center px-8 bg-foreground text-background shadow
-                hidden sm:flex absolute
-            ">
-                <h1 className="text-2xl font-black font-mono">Shuttr.</h1>
-                <nav className="flex justify-center items-center gap-8">
-                    <Link href="/">
-                        <button className="cursor-pointer">Home</button>
-                    </Link>
+            <SidebarProvider open={false}>
+                <AppSidebar />
 
-                    <Link href="/about">
-                        <button className="cursor-pointer">About</button>
-                    </Link>
+                <header className="w-full py-4 h-16 justify-between items-center px-4 md:px-8 bg-foreground text-background shadow
+                    flex absolute
+                ">
+                    <h1 className="text-xl md:text-2xl font-black font-mono">Shuttr.</h1>
 
-                    <Link href="#">
-                        <button className="cursor-pointer">Soon</button>
-                    </Link>
-                </nav>
-                <h1 className="opacity-0 text-2xl font-black font-mono">Shuttr.</h1>
-            </header>
-            <main className="w-full h-dvh pt-16">
-                {children}
-            </main>
-        </>
+                    {/* visible on desktop */}
+                    <nav className="justify-center items-center gap-8 hidden md:flex">
+
+                        <Link href="/">
+                            <button className="cursor-pointer">Home</button>
+                        </Link>
+
+                        <Link href="/about">
+                            <button className="cursor-pointer">About</button>
+                        </Link>
+                    
+                    </nav>
+
+                    {/* visible on mobile */}
+                    <div className="md:hidden text-2xl font-black font-mono">
+                        <SidebarTrigger>
+                            <Button variant="ghost">
+                                <Menu className="size-max"/>
+                            </Button>
+                        </SidebarTrigger>
+                    </div>
+                </header>
+                
+                <main className="w-full h-dvh pt-16">
+                    {children}
+                </main>
+
+            </SidebarProvider>
+        </> 
     )
 }
